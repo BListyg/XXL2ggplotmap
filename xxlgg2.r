94deb355e827c51b614f093641b581d684a57084
Benjamin Listyg <listyg.ben@gmail.com>
2018-04-24 15:18:57 -0500

    Updated for 2018

--- xxlgg2.r
+++ xxlgg2.r
@@ -4,19 +4,39 @@
 library(rvest)
 library(reshape2)
 library(ggmap)
+library(tm)
 
 #Linking to the the voting website for all the nominees
 links = read_html("http://freshmen.xxlmag.com/10th-spot/")
 
 link = links %>% 
   #specifying the HTML Node
-  html_nodes(".desc2") %>% 
+  html_nodes(".list-description-secondary") %>% 
   html_text()
+
+link = trimws(link, which = c("both"))
+
 link = melt(link)
 link = table(link)
 link = melt(link)
+
+link$link <- as.character(link$link)
+
 #adding on the longitude and latitude to the rapper's name and where they're from
-link = cbind(geocode(as.character(link$link)),link)
+
+link$link <- removePunctuation(link$link)
+
+link$link <- toupper(link$link)
+
+link$link <- gsub("CALIF", 
+                  "CALIFORNIA", 
+                  link$link)
+
+link$link <- gsub("MASS", 
+                  "Massachusetts", 
+                  link$link)
+
+link = cbind(geocode(as.character(link$link), source = 'dsk',force = ifelse(source == "dsk", FALSE, TRUE)),link)
 
 #Naming the columns of the rapper's city and how many come from there so there isn't any column confusion
 colnames(link)[3] = paste("RapperArea")
@@ -31,9 +51,9 @@ ggplot() + geom_polygon(data = usa, aes(x=long, y = lat, group = group)) +
   coord_map() +
   #making it red because that's XXL's color scheme
   geom_point(data=link, aes(x=lon, y=lat, size=RapperCount), color="red") +
-  xlim(-130,-60)
+  xlim(-130,-60) + ylim(10,50) 
 
 #####################################
 #XXL Link = http://freshmen.xxlmag.com/10th-spot/
-  
+
 #XXL HTML Node = .desc2
